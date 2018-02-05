a4d78bfdc9e56ad1ab1fd56f125291bf7cbd0b05
Peter Williams <peter.williams@papercut.com>
2018-02-05 17:02:11 +1100

    Compute AUC

--- toxic/clf_glove_nb_spacy.py
+++ toxic/clf_glove_nb_spacy.py
@@ -14,7 +14,6 @@
     topic. Here is a link to the section of the video which discusses this:
     [Naive Bayes video](https://youtu.be/37sFIak42Sc?t=3745).
 """
-import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 import os
@@ -23,35 +22,10 @@ import time
 from utils import COMMENT, save_json, load_json, save_pickle, load_pickle
 
 
-N_GRAM = 3
 SPACY_VECTOR_SIZE = 300  # To match SpaCY vectors
 SPACY_VECTOR_TYPE = np.float32
-
-
-
-
-if False:
-    text = "Blah blah. What's that?"
-    tokens = tokenize(text, token_vector)
-    print(tokens)
-    print(sorted(token_vector))
-    print([(v.shape, v.dtype) for v in token_vector.values()])
-    assert False
-
-
-
-
-# ## Building the model
-#
-# We'll start by creating a *bag of words* representation, as a *term document matrix*. We'll use
-# ngrams, as suggested in the NBSVM paper.
-if False:
-    s = '''It turns out "why is" that's using. Doesn't it? Can't i'''
-    t = tokenize(s)
-    print(t)
-    assert False
-
-
+SAVE_TIME = 60.0    # Save every 60 secs
+SAVE_ITEMS = 2000   # Save if number of items in cache increases by this much
 
 
 # Here's the basic naive bayes feature equation:
@@ -82,9 +56,10 @@ def fit_model(x, y):
 
 class ClfGloveNBSpace:
 
-    def __init__(self, label_cols, model_dir):
+    def __init__(self, label_cols, model_dir, n_gram):
         self.label_cols = label_cols
         self.model_dir = model_dir
+        self.n_gram = n_gram
         self.m = {}
         self.r = {}
         self.text_tokens_path = os.path.join(self.model_dir, 'text.tokens.json')
@@ -95,14 +70,17 @@ class ClfGloveNBSpace:
         self.token_vector_len = len(self.token_vector)
         self.nlp = spacy.load('en_core_web_lg')
 
-    def _save(self):
-        print('_save 1:', self.text_tokens_len, len(self.text_tokens), self.text_tokens_path)
-        print('_save2:', self.token_vector_len, len(self.token_vector), self.token_vector_path)
-
-        if self.text_tokens_len != len(self.text_tokens):
+    def _save(self, min_delta=0):
+        if self.text_tokens_len + min_delta < len(self.text_tokens):
+            print('_save 1: %7d = %7d + %4d %s' % (len(self.text_tokens),
+                self.text_tokens_len, len(self.text_tokens) - self.text_tokens_len,
+                self.text_tokens_path))
             save_json(self.text_tokens_path, self.text_tokens)
             self.text_tokens_len = len(self.text_tokens)
-        if self.token_vector_len != len(self.token_vector):
+        if self.token_vector_len + 2 * min_delta < len(self.token_vector):
+            print('_save 2: %7d = %7d + %4d %s' % (len(self.token_vector),
+                self.token_vector_len, len(self.token_vector) - self.token_vector_len,
+                self.token_vector_path))
             save_pickle(self.token_vector_path, self.token_vector)
             self.token_vector_len = len(self.token_vector)
 
@@ -119,18 +97,25 @@ class ClfGloveNBSpace:
         """Return a list of token lists
             Each token list corresponds to a row of df
         """
-        t0 = time.clock()
+        print('tokenize_df: %d %d' % (len(df), self.text_tokens_len))
+        t0 = t1 = time.clock()
         tokens_list = []
-        for text in df[COMMENT]:
+        n_tokenized = 0
+        for i, text in enumerate(df[COMMENT]):
             tokens = self.text_tokens.get(text)
             if not tokens:
                 tokens = self.tokenize(text)
                 self.text_tokens[text] = tokens
+                n_tokenized += 1
             tokens_list.append(tokens)
-        print('tokens: %d %1.f sec %.2f sec / token' % (
-            len(tokens),
-            time.clock() - t0,
-            (time.clock() - t0) / len(tokens)))
+            t = time.clock()
+            if t > t1 + SAVE_TIME or i + 1 == len(df):
+                print('tokenize_df: %d texts, %d new, %1.f sec (%.2f sec / token)' % (
+                    i + 1, n_tokenized,
+                    time.clock() - t0,
+                    (time.clock() - t0) / max(n_tokenized, 1)))
+                t1 = t
+                self._save(min_delta=SAVE_ITEMS)
         self._save()
         return tokens_list
 
@@ -138,27 +123,24 @@ class ClfGloveNBSpace:
         """Compute an embedding vector for all n-grams in token_list
         """
         vec = np.zeros((n, SPACY_VECTOR_SIZE), dtype=np.float64)
-        n_vecs = len(token_list) - n + 1
-        for i in range(n_vecs):
-            for j in range(n):
+        n_toks = len(token_list)
+        for i in range(n_toks):
+            for j in range(min(n, n_toks - i)):
                 vec[j] += self.token_vector[token_list[i + j]]
-        vec /= n_vecs
+        vec /= n_toks
         return np.reshape(vec, n * SPACY_VECTOR_SIZE)
 
-    def compute_ngram_matrix(self, token_matrix, n):
-        print('@@', type(token_matrix),  type(token_matrix[0]))
-        mat = np.zeros((len(token_matrix), n * SPACY_VECTOR_SIZE), dtype=np.float64)
+    def compute_ngram_matrix(self, token_matrix):
+        mat = np.zeros((len(token_matrix), self.n_gram * SPACY_VECTOR_SIZE), dtype=np.float64)
         for i, token_list in enumerate(token_matrix):
-            assert isinstance(token_list, list), (i, token_list)
-            print(i, type(token_list), token_list)
-            mat[i, :] = self.compute_ngram_vector(token_list, n)
+            mat[i, :] = self.compute_ngram_vector(token_list, self.n_gram)
         return mat
 
     def fit(self, train):
         t0 = time.clock()
         train_tokens = self.tokenize_df(train)
         print('train_tokens: %1.f sec %.2f sec / token' % (time.clock() - t0, (time.clock() - t0) / len(train_tokens)))
-        x = self.compute_ngram_matrix(train_tokens, N_GRAM)
+        x = self.compute_ngram_matrix(train_tokens)
         for i, col in enumerate(self.label_cols):
             self.m[col], self.r[col] = fit_model(x, train[col])
 
@@ -166,12 +148,10 @@ class ClfGloveNBSpace:
         label_cols = self.label_cols
         preds = np.zeros((len(test), len(label_cols)))
         test_tokens = self.tokenize_df(test)
-        test_x = self.compute_ngram_matrix(test_tokens, N_GRAM)
+        test_x = self.compute_ngram_matrix(test_tokens)
         for i, col in enumerate(self.label_cols):
-            print('^' * 80)
             print('fit', i, col)
             m, r = self.m[col], self.r[col]
             x_nb = test_x * r
             preds[:, i] = m.predict_proba(x_nb)[:, 1]
         return preds
-
--- toxic/framework.py
+++ toxic/framework.py
@@ -2,9 +2,12 @@
 """
     Framework for evaluating and submitting models to Kaggle Toxic Comment challenge
 """
+import numpy as np
 import pandas as pd
 import os
+import random
 from os.path import expanduser, join
+from sklearn.metrics import roc_auc_score
 import matplotlib.pyplot as plt
 from utils import COMMENT
 from clf_glove_nb_spacy import ClfGloveNBSpace
@@ -12,10 +15,12 @@ from clf_glove_nb_spacy import ClfGloveNBSpace
 
 VERBOSE = False
 GRAPHS = False
-N_SAMPLES = 100  # > 0 for testing
+N_GRAM = 1
+N_SAMPLES = 50000  # > 0 for testing
 MODEL = ClfGloveNBSpace
 MODEL_DIR = 'model.%s' % MODEL.__name__
 SUBMISSION_NAME = 'submissionxxx.csv'
+SEED = 234
 
 print('MODEL_DIR=%s' % MODEL_DIR)
 
@@ -30,6 +35,8 @@ if N_SAMPLES > 0:
     train = train[:N_SAMPLES]
     test = test[:N_SAMPLES]
 
+random.seed(SEED)
+np.random.seed(SEED)
 
 # ## Looking at the data
 #
@@ -65,11 +72,41 @@ print('train=%d test=%d (%.1f%%)' % (len(train), len(test), 100.0 * len(test) / 
 train[COMMENT].fillna("unknown", inplace=True)
 test[COMMENT].fillna("unknown", inplace=True)
 
-clf = MODEL(label_cols, MODEL_DIR)
-clf.fit(train)
-preds = clf.predict(test)
 
-# And finally, create the submission file.
-submid = pd.DataFrame({'id': subm['id']})
-submission = pd.concat([submid, pd.DataFrame(preds, columns=label_cols)], axis=1)
-submission.to_csv(SUBMISSION_NAME, index=False)
+def split_data(df, frac):
+    # indexes = list(range(len(df)))
+    indexes = list(df.index)
+    random.shuffle(indexes)
+    n = int(len(df) * frac)
+    train = df.loc[indexes[:n]]
+    test = df.loc[indexes[n:]]
+    return train, test
+
+
+def make_submission(train, test):
+    clf = MODEL(label_cols, MODEL_DIR, N_GRAM)
+    clf.fit(train)
+    preds = clf.predict(test)
+
+    # And finally, create the submission file.
+    submid = pd.DataFrame({'id': subm['id']})
+    submission = pd.concat([submid, pd.DataFrame(preds, columns=label_cols)], axis=1)
+    submission.to_csv(SUBMISSION_NAME, index=False)
+
+
+def evaluate(train0):
+    train, test = split_data(train0, 0.7)
+    clf = MODEL(label_cols, MODEL_DIR, N_GRAM)
+    clf.fit(train)
+    preds = clf.predict(test)
+    auc = np.zeros(len(label_cols), dtype=np.float64)
+    for i, col in enumerate(label_cols):
+        y_true = test[col]
+        y_pred = preds[:, i]
+        auc[i] = roc_auc_score(y_true, y_pred)
+    mean_auc = auc.mean()
+    print('auc=%.3f' % mean_auc)
+
+
+# make_submission(train, test)
+evaluate(train)
