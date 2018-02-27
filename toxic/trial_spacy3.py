4f85b8d335f6c9616bf86c5c3e968a9ccbc6bbcc
Peter Williams <peter.williams@papercut.com>
2018-02-27 15:52:06 +1100

    more parameter trials

--- toxic/trial_spacy3.py
+++ toxic/trial_spacy3.py
@@ -7,7 +7,7 @@ from framework import Evaluator, seed_random, show_auc
 from clf_spacy import ClfSpacy
 
 
-submission_name = 'spacy_lstm8'
+submission_name = 'spacy_lstm10'
 epochs = 40
 
 
@@ -60,8 +60,46 @@ def get_clf6():
                     lstm_type=lstm_type)
 
 
+def get_clf11():
+    return ClfSpacy(n_hidden=192, max_length=100,  # Shape
+                    dropout=0.5, learn_rate=0.001,  # General NN config
+                    epochs=epochs, batch_size=150, frozen=frozen,
+                    lstm_type=lstm_type)
+
+
+def get_clf12():
+    return ClfSpacy(n_hidden=256, max_length=100,  # Shape
+                    dropout=0.5, learn_rate=0.001,  # General NN config
+                    epochs=epochs, batch_size=150, frozen=frozen,
+                    lstm_type=lstm_type)
+
+
+def get_clf13():
+    return ClfSpacy(n_hidden=128, max_length=100,  # Shape
+                    dropout=0.5, learn_rate=0.002,  # General NN config
+                    epochs=epochs, batch_size=150, frozen=frozen,
+                    lstm_type=lstm_type)
+
+
+def get_clf14():
+    return ClfSpacy(n_hidden=128, max_length=100,  # Shape
+                    dropout=0.5, learn_rate=0.005,  # General NN config
+                    epochs=epochs, batch_size=150, frozen=frozen,
+                    lstm_type=lstm_type)
+
+
+def get_clf15():
+    return ClfSpacy(n_hidden=128, max_length=100,  # Shape
+                    dropout=0.5, learn_rate=0.010,  # General NN config
+                    epochs=epochs, batch_size=150, frozen=frozen,
+                    lstm_type=lstm_type)
+
+
 xprint_init(submission_name, False)
-clf_list = [get_clf1, get_clf0, get_clf2, get_clf3, get_clf4, get_clf5]
+clf_list = [get_clf11, get_clf12, get_clf13, get_clf14, get_clf15,
+           # get_clf0, get_clf2, get_clf3,
+           get_clf4, get_clf5,
+           get_clf1]
 # clf_list.reverse()
 auc_list = []
 frozen = True
@@ -74,14 +112,20 @@ frozen = True
 # clf=0.9721   0: get_clf0 ClfSpacy(batch_size=150, dropout=0.5, learn_rate=0.001, lstm_type=4, max_length=100, n_examples=-1, n_hidden=64)
 # clf=0.9708   5: get_clf1 ClfSpacy(batch_size=150, dropout=0.5, learn_rate=0.001, lstm_type=3, max_length=100, n_examples=-1, n_hidden=128)
 
+# RESULTS SUMMARY: 4
+# auc=0.9760   0: get_clf1 ClfSpacy(batch_size=150, dropout=0.5, learn_rate=0.001, lstm_type=2, max_length=100, n_examples=-1, n_hidden=128)
+# auc=0.9730   1: get_clf0 ClfSpacy(batch_size=150, dropout=0.5, learn_rate=0.001, lstm_type=2, max_length=100, n_examples=-1, n_hidden=64)
+# auc=0.9725   2: get_clf2 ClfSpacy(batch_size=150, dropout=0.5, learn_rate=0.001, lstm_type=2, max_length=150, n_examples=-1, n_hidden=64)
+# auc=0.9670   3: get_clf3 ClfSpacy(batch_size=150, dropout=0.2, learn_rate=0.001, lstm_type=2, max_length=100, n_examples=-1, n_hidden=64)
+
 for lstm_type in (2, 8, 7, 6, 5):  # , 3, 4, 1):
     for get_clf in clf_list:
         xprint('#' * 80)
         xprint(get_clf())
         seed_random(seed=1234)
-        evaluator = Evaluator(n=2)
-        ok, auc = evaluator.evaluate(get_clf)
-        auc_list.append((auc, get_clf.__name__, str(get_clf())))
+        evaluator = Evaluator(n=1)
+        ok, auc0 = evaluator.evaluate(get_clf)
+        auc_list.append((auc0, get_clf.__name__, str(get_clf())))
         results = [(i, auc, clf, clf_str) for i, (auc, clf, clf_str) in enumerate(auc_list)]
         results.sort(key=lambda x: (-x[1].mean(), x[2], x[3]))
         xprint('~' * 100)
