073d0868fbc4fbe2052a6a92b1dcc7f0efe16797
Peter Williams <peter.williams@papercut.com>
2018-03-06 08:52:13 +1100

    preparing trial_spacy9_submit.py submission

--- toxic/run9s.sh
+++ toxic/run9s.sh
@@ -2,13 +2,13 @@
 trap "echo Program crashed. Restarting" ERR
 
 
-while [ ! -f ccompleted.spacy_lstmx_90.txt ]
+while [ ! -f completed.spacy_lstmx_90.txt ]
 do
     python trial_spacy9_submit.py
     echo sleeping 1
     sleep 5
     echo sleeping 2
-    sleep 5
+    sleep 100
 done
 
 echo done
