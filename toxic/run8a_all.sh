36db82509e013d8ed74ab4c8f87bd0005ab337fc
Peter Williams <peter.williams@papercut.com>
2018-03-09 10:13:42 +1100

    case sensitivity on ubuntu is different to mac

--- toxic/run8a_all.sh
+++ toxic/run8a_all.sh
@@ -4,7 +4,7 @@ trap "echo Program crashed. Restarting" ERR
 
 while [ ! -f completed.e_spacy_lstmx_85a_ALL.txt ]
 do
-    python trial_spacy8a_all.py
+    python trial_spacy8a_ALL.py
     echo sleeping 1
     sleep 5
     echo sleeping 2
